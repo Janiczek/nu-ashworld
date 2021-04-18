a6f758dfeb20cfe5ea7c6bf3ef2df96d7d900b57
Martin Janiczek <martin@janiczek.cz>
2021-04-18 23:42:41 +0200

    Sorting tiebreakers

--- src/Data/Ladder.elm
+++ src/Data/Ladder.elm
@@ -1,13 +1,36 @@
 module Data.Ladder exposing (sort, sortMixed)
 
 import Data.Player as Player exposing (COtherPlayer, CPlayer)
+import Sort exposing (Sorter)
 
 
-sort : List { p | xp : Int } -> List { p | xp : Int }
+type alias Sortable p =
+    { p | xp : Int, wins : Int, losses : Int, caps : Int }
+
+
+sorter : Sorter (Sortable p)
+sorter =
+    let
+        byXp : Sorter (Sortable p)
+        byXp =
+            Sort.reverse (Sort.by .xp Sort.increasing)
+
+        byWinsLossesRatio : Sorter (Sortable p)
+        byWinsLossesRatio =
+            Sort.reverse (Sort.by (\{ wins, losses } -> toFloat wins / toFloat losses) Sort.increasing)
+
+        byCaps : Sorter (Sortable p)
+        byCaps =
+            Sort.reverse (Sort.by .caps Sort.increasing)
+    in
+    byXp
+        |> Sort.tiebreaker byWinsLossesRatio
+        |> Sort.tiebreaker byCaps
+
+
+sort : List (Sortable p) -> List (Sortable p)
 sort players =
-    -- TODO tie-breakers? W/L ratio? caps?
-    players
-        |> List.sortBy (negate << .xp)
+    Sort.list sorter players
 
 
 sortMixed :
