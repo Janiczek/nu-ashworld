94b8cde925600042cff7e206f8377d83490592db
Martin Janiczek <martin@janiczek.cz>
2021-04-28 00:31:26 +0200

    New env field

--- src/Env.elm
+++ src/Env.elm
@@ -8,3 +8,8 @@ adminPasswordHash : String
 adminPasswordHash =
     -- Sha256.sha256 ""
     "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+
+loggingApiKey : String
+loggingApiKey =
+    "ignore-this-api-key"
