dc2bac79d0d740a396c00f8388c54aae3cd50464
Martin Janiczek <martin@janiczek.cz>
2021-04-29 23:32:05 +0200

    File

--- src/Evergreen/V81/Types.elm
+++ src/Evergreen/V81/Types.elm
@@ -21,7 +21,7 @@ import Evergreen.V81.Data.Trait
 import Evergreen.V81.Data.Vendor
 import Evergreen.V81.Data.World
 import Evergreen.V81.Frontend.Route
-import File
+import File exposing (File)
 import Lamdera
 import Set
 import Time
